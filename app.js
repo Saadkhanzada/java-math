1a29b43e6b47fad25f06690f04463d9fecc08166
Saad Khan <100066385+Saadkhanzada@users.noreply.github.com>
2022-04-19 15:57:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+// ------------math method-----------
+
+
+// --------question 1
+
+// var number = 5.13213212
+
+// var roundValue = Math.round(number)
+// console.log(roundValue)
+
+// var floor = Math.floor(number)
+// console.log(floor)
+
+// var ceil = Math.ceil(number)
+// console.log(ceil)
+
+
+// --------question 2
+
+//  var number = -2.673
+
+//  var roundValue = Math.round(number)
+//  console.log(roundValue)
+
+//  var floor = Math.floor(number)
+//  console.log(floor)
+
+//  var ceil = Math.ceil(number)
+//  console.log(ceil)
+
+// --------question 3
+
+// var bs = Math.abs(-4,4)
+// console.log(bs)
+
+// var bs = Math.abs(-5,5)
+// console.log(bs)
+
+
+// --------question 4
+
+// var random = Math.floor(Math.random() * 6 + 1);
+
+// document.write(random)
+
+// --------question 5
+
+// --------question 6
+
+// var random = Math.floor(Math.random() * 100 + 1);
+
+// document.write(random)
+
+// --------question 7
+
+// var userInput = prompt("Enter your weight in kilogram")
+
+// if(userInput == 50){
+//     document.write("The user weight is " + userInput +  "kilogram") 
+// }else{
+//     userInput = false
+//     alert("your weight is to high")
+// }
+// if(userInput == 50.2){
+//     document.write("The user weight is " + userInput +  "kilogram") 
+// }else{
+//     userInput = false
+//     alert("your weight is to high")
+// }
+// if(userInput == 50.5){
+//     document.write("The user weight is " + userInput +  "kilogram") 
+// }else{
+//     userInput = false
+//     alert("your weight is to high")
+// }
+// --------question 8
+
+// var userInput = prompt("enter a number between 1 to 10")
+// for(var i = 1; i <=10; i++){
+
+//     if(userInput){
+//         userInput = i
+//         alert("Congratulation")
+//         break
+//     }else{
+//         userInput = ""
+//         alert("try again")
+//         break
+//     }
+
+// }
